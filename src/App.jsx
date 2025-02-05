6af35b6801136f96c471540c83f57c729d6f2e53
ProfGregory <gveloso@findes.org.br>
2025-02-05 15:56:06 -0300

    Segundo commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <img src="https://lncimg.lance.com.br/cdn-cgi/image/width=828,quality=75,fit=pad,format=webp/uploads/2024/09/WhatsApp-Image-2024-09-08-at-18.02.46-aspect-ratio-512-320-1.jpeg" alt="" width={500} height={300} />
+      <h1>The best in the World</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione quos laborum iste perferendis accusantium mollitia consequuntur molestias. Nam numquam, perspiciatis dolore cum, recusandae illo sequi, eum eos fugit amet est. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit laborum quo doloribus quibusdam excepturi deleniti nesciunt error! Doloremque molestiae consequatur molestias impedit deleniti veniam, eum numquam voluptates fugit at obcaecati? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure dolor autem qui illo velit dolores, aut officiis est eum officia nemo labore consequuntur provident commodi impedit! Accusamus vitae impedit rerum!</p>
+    </>
+  )
+}
+
+export default App
